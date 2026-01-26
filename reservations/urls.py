acd0570a641ad0c6f196c6f7e94f4ce09fea8b36
Masamba <masamba.randy@gmail.com>
2026-01-26 21:43:06 +0100

    Auth: homepage returns 404 (root URL not configured yet)

--- reservations/urls.py
+++ reservations/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 #from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
 
 
 urlpatterns = [
     #path('', RedirectView.as_view(url='/artist/', permanent=False)),
     path('catalogue/', include('catalogue.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
