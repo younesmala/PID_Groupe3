51ea24196f11c1dc2cb357d0aa16b6daaa0d4c9e
Younes <younesmala124@gmail.com>
2026-01-28 16:20:53 +0100

    feat: secure artists CRUD, auth flow and catalogue navigation (MVP)

--- reservations/settings.py
+++ reservations/settings.py
@@ -138,5 +138,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
-LOGIN_URL = "login"
+LOGIN_URL = "accounts:login"
+
 
--- reservations/urls.py
+++ reservations/urls.py
@@ -5,12 +5,12 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # Accueil du site
+    # Accueil du SITE (page racine /)
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 
-    # Catalogue
+    # Catalogue (toutes les routes sous /catalogue/)
     path("catalogue/", include(("catalogue.urls", "catalogue"), namespace="catalogue")),
 
-    # Auth Django
-    path("accounts/", include("django.contrib.auth.urls")),
+    # Auth (login/logout/password_reset...) sous /accounts/ (avec namespace accounts)
+    path("accounts/", include(("django.contrib.auth.urls", "accounts"), namespace="accounts")),
 ]
