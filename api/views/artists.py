7058da42654fe599b35aa05d3684b64e26bf4e76
Masamba <masamba.randy@gmail.com>
2026-03-17 18:58:19 +0100

    feat: add GET and POST /api/artists endpoints

--- api/views/artists.py
+++ api/views/artists.py
@@ -5,15 +5,22 @@ from rest_framework import status
 from catalogue.models import Artist
 from api.serializers.artists import ArtistSerializer
 
+
 class ArtistsView(APIView):
-    """
-    GET /api/artists/
-    """
 
     def get(self, request):
         qs = Artist.objects.all()
         serializer = ArtistSerializer(qs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ArtistSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ArtistsDetailView(APIView):
     def get(self, request, *args, **kwargs):
