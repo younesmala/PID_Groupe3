8235cf5c68caf4459aedd4a30d53afadf303c99b
Masamba <masamba.randy@gmail.com>
2026-03-17 18:16:46 +0100

    feat: add GET /api/artists endpoint

--- api/serializers/artist_types.py
+++ api/serializers/artist_types.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+from catalogue.models import ArtistType
 
-class ArtistTypeSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    artist_id = serializers.IntegerField()
-    type_id = serializers.IntegerField()
-    # Ajoutez d'autres champs selon le modèle ArtistType
+
+class ArtistTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArtistType
+        fields = ["id", "artist", "type"]
--- api/views/artists.py
+++ api/views/artists.py
@@ -1,18 +1,19 @@
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from catalogue.models import Artist
+from api.serializers.artists import ArtistSerializer
 
 class ArtistsView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def post(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def put(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def delete(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
+    """
+    GET /api/artists/
+    """
+
+    def get(self, request):
+        qs = Artist.objects.all()
+        serializer = ArtistSerializer(qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class ArtistsDetailView(APIView):
     def get(self, request, *args, **kwargs):
