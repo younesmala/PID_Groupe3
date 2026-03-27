64b3074c8eaf69ad7c720585db325c4fc4ee7f9d
Oumar Barry <barry.oumar89@gmail.com>
2026-03-27 17:23:31 +0100

    Preparation de serializer pour la reation de l'API representations

--- api/serializers/representations.py
+++ api/serializers/representations.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from catalogue.models import Representation
 
+
 class RepresentationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Representation
-        fields = '__all__'
+        fields = ["id", "show", "schedule", "location"]
