cfee7a51bbaf621ab4dff22e2617ca0b0200a5b4
lierneuxnicolas <lierneuxnicolas@outlook.com>
2026-03-31 15:01:41 +0200

    add artists feature base structure in frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import ArtistsList from "./pages/ArtistsList";
+
+function App() {
+  return <ArtistsList />;
+}
+
+export default App;
---
+++ frontend/src/pages/ArtistDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArtistDetail = () => {
+  return (
+    <div>
+      <h1>Artist Detail</h1>
+    </div>
+  );
+};
+
+export default ArtistDetail;
