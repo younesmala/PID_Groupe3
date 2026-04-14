6e75cba9248d20ef593a89bf4dd73478416f84ea
Oumar Barry <barry.oumar89@gmail.com>
2026-04-14 19:57:36 +0200

    Mise à jour views.py pour créer réservation depuis panier

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .cart import Cart
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, "cart/cart_detail.html", {"cart": cart})
