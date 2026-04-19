09947c486fd047302f053dff661bc8ba6144b614
masambarandy-jpg <masamba.randy@gmail.com>
2026-04-19 23:45:50 +0200

    fix: ajout route admin Django

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
 from django.urls import path, include
-
 from catalogue.views.home import home as catalogue_home
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
 ]
 
