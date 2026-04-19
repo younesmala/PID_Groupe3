ddbf9a12132c13ff5552e5f0be5613b47e48a485
lierneuxnicolas <lierneuxnicolas@outlook.com>
2026-04-19 09:18:35 +0200

    i18n + traduction + templates modifiés

--- catalogue/forms/ShowForm.py
+++ catalogue/forms/ShowForm.py
@@ -1,7 +1,18 @@
 from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
 from catalogue.models import Show
 
 class ShowForm(ModelForm):
     class Meta:
         model = Show
         fields = ['slug', 'title', 'description', 'poster_url', 'duration', 'created_in', 'location', 'bookable']
+        labels = {
+            'slug': _('Slug'),
+            'title': _('Titre'),
+            'description': _('Description'),
+            'poster_url': _("URL de l'affiche"),
+            'duration': _('Durée'),
+            'created_in': _('Créé en'),
+            'location': _('Lieu'),
+            'bookable': _('Réservable'),
+        }
