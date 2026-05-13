dee91a26145668376523a5a8161222a81cd89d0e
Younes <younesmala124@gmail.com>
2026-05-13 12:19:50 +0200

    correction routes et detail admin spectacles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -336,15 +336,7 @@ function AppContent() {
           }
         />
         <Route path="/search" element={<Search />} />
-        {/* Redirection si l'utilisateur tente d'aller sur /login manuellement */}
-        <Route 
-          path="/login" 
-          element={
-            <Navigate to="/" replace onRender={() => {
-              window.dispatchEvent(new Event("open-login-modal"));
-            }} />
-          } 
-        />
+        <Route path="/:lang/search" element={<Search />} />
         <Route path="/reviews" element={<Reviews />} />
         <Route path="/:lang/reviews" element={<Reviews />} />
         <Route
